5314e73f9fe83ba252c8490cf695c984fbc9cda9
cihaderoll <cihaderoll@gmail.com>
2023-04-30 15:11:36 +0300

    functions start

--- src/test.ts
+++ src/test.ts
@@ -2,8 +2,14 @@ let greet = (name: string, age: number) =>{
     console.log("deneme");
     console.log(`your age: ${age}`);
     console.log("name: " + name);
-    //deneme commit
+
     return age;
 }
 
 let result = greet("afsf", 23);
+
+const sum = (a: number, b: number, c: number | string = '10') => {
+    console.log("func!!");
+}
+
+sum(4, 6);
